bac84add8c3fe0f6603e37303cb7be6b6f7ed41b
DocRo93 <rnrathod93@yahoo.co.uk>
2026-08-12 14:06:03 +0100

    initial commit

---
+++ ConsoleCalculatorApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        //Display
+        Console.WriteLine("================================");
+        Console.WriteLine(" ");
+        Console.WriteLine("C# CONSOLE CALCULATOR");
+        Console.WriteLine(" ");
+        Console.WriteLine("================================");
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        //menu
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Modulus");
+        Console.WriteLine("6. Exit");
+        //Input instructions
+        Console.WriteLine(" ");
+        Console.WriteLine("Select an option:");
+        Console.WriteLine(" ");
+        Console.WriteLine("Enter first number");
+        Console.WriteLine("Enter second number");
+        Console.WriteLine(" ");
+        Console.WriteLine("================================");
+        Console.WriteLine(" ");
+        Console.WriteLine("================================");
+
+
+
+    }
+}
