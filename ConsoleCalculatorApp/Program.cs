8cf983f1663a31cb26ae5fe9e1e891a782a4ce20
DocRo93 <rnrathod93@yahoo.co.uk>
2026-08-13 09:12:25 +0100

    added operation selection if methods and modulus method

--- ConsoleCalculatorApp/Program.cs
+++ ConsoleCalculatorApp/Program.cs
@@ -8,101 +8,150 @@ class Program
         int option = 0;
         decimal number1 = 0;
         decimal number2 = 0;
+        bool systemExit = false;
 
-        //Display
-        Console.WriteLine("================================");
-        Console.WriteLine(" ");
-        Console.WriteLine("C# CONSOLE CALCULATOR");
-        Console.WriteLine(" ");
-        Console.WriteLine("================================");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        //menu
-        Console.WriteLine("1. Addition");
-        Console.WriteLine("2. Subtraction");
-        Console.WriteLine("3. Multiplication");
-        Console.WriteLine("4. Division");
-        Console.WriteLine("5. Modulus");
-        Console.WriteLine("6. Exit");
-       
-        
-        //Input instructions
-        Console.WriteLine(" ");
-        
-        //select option
-        Console.WriteLine("Select an option:");
-        string? inputSelection = Console.ReadLine();
-        validInput = false;
-        while (!validInput)
+
+        while (!systemExit)
         {
-            if (int.TryParse(inputSelection, out option))
+
+            Console.Clear();
+
+            //Display
+            Console.WriteLine("================================");
+            Console.WriteLine(" ");
+            Console.WriteLine("C# CONSOLE CALCULATOR");
+            Console.WriteLine(" ");
+            Console.WriteLine("================================");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            //menu
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Modulus");
+            Console.WriteLine("6. Exit");
+
+
+            //Input instructions
+            Console.WriteLine(" ");
+
+            //select option
+            Console.WriteLine("Select an option:");
+            string? inputSelection = Console.ReadLine();
+            validInput = false;
+
+            while (!validInput)
             {
-                validInput = true;
+                if (int.TryParse(inputSelection, out option))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again!");
+                    inputSelection = Console.ReadLine();
+                    validInput = false;
+                }
             }
-            else
+
+            //check for exit
+            if (option == 6)
             {
-                Console.WriteLine("Error, try again!");
-                inputSelection = Console.ReadLine();
-                validInput = false;
+                systemExit = true;
+                Console.WriteLine("================================");
+                Console.WriteLine(" ");
+                Console.WriteLine("------Thank you for using-------");
+                Console.WriteLine(" ");
+                Console.WriteLine("-----C# CONSOLE CALCULATOR------");
+                Console.WriteLine(" ");
+                Console.WriteLine("================================");
+                continue;
             }
+
+            Console.WriteLine(" ");
+
+            //User input - number 1
+            Console.WriteLine("Enter first number");
+            string? inputNumber1 = Console.ReadLine();
+            validInput = false;
+
+            while (!validInput)
+            {
+                if (decimal.TryParse(inputNumber1, out number1))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again!");
+                    inputNumber1 = Console.ReadLine();
+                    validInput = false;
+                }
+            }
+
+            //User input - number 2
+            Console.WriteLine("Enter second number");
+            string? inputNumber2 = Console.ReadLine();
+            validInput = false;
+
+            while (!validInput)
+            {
+                if (decimal.TryParse(inputNumber2, out number2))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again!");
+                    inputNumber2 = Console.ReadLine();
+                    validInput = false;
+                }
+            }
+
+            // Output
+            Console.WriteLine(" ");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine(" ");
+
+            //Operation selection
+            if (option == 1)
+            {
+                Console.WriteLine("Answer:");
+                Console.WriteLine($"{number1} + {number2} = {add(number1, number2)}");
+            } else if (option == 2){
+                Console.WriteLine("Answer:");
+                Console.WriteLine($"{number1} - {number2} = {subtract(number1, number2)}");
+            } else if (option == 3)
+            {
+                Console.WriteLine("Answer:");
+                Console.WriteLine($"{number1} x {number2} = {multiply(number1, number2)}");
+            } else if (option == 4)
+            {
+                Console.WriteLine("Answer:");
+                Console.WriteLine($"{number1} / {number2} = {divide(number1, number2)}");
+            } else if (option == 5)
+            {
+                Console.WriteLine("Answer:");
+                Console.WriteLine($"{number1} % {number2} = {modulus(number1, number2)}");
+            }
+            else if (option == 6) {
+                systemExit = true;
+                break;
+            } else
+            {
+                Console.WriteLine("Please only choose an option between 1-6");
+                continue;
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("================================");
+            Console.WriteLine("================================");
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to return to the menu...");
+            Console.ReadKey();
+
         }
-        Console.WriteLine(" ");
-
-        //User input - number 1
-        Console.WriteLine("Enter first number");
-        string? inputNumber1 = Console.ReadLine();
-        validInput = false;
-
-        while (!validInput)
-        {
-            if (decimal.TryParse(inputNumber1, out number1))
-            {
-                validInput = true;
-            }
-            else
-            {
-                Console.WriteLine("Error, try again!");
-                inputNumber1 = Console.ReadLine();
-                validInput = false;
-            }
-        }
-        
-        //User input - number 2
-        Console.WriteLine("Enter second number");
-        string? inputNumber2 = Console.ReadLine();
-        validInput = false;
-
-        while (!validInput)
-        {
-            if (decimal.TryParse(inputNumber2, out number2))
-            {
-                validInput = true;
-            }
-            else
-            {
-                Console.WriteLine("Error, try again!");
-                inputNumber2 = Console.ReadLine();
-                validInput = false;
-            }
-        }
-
-        // Output
-        Console.WriteLine(" ");
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine(" ");
-
-
-        if (option == 1)
-        {
-            Console.WriteLine("Answer:");
-            Console.WriteLine($"{number1} + {number2} = {add(number1,number2)}");
-        }
-     
-        Console.WriteLine(" ");
-        Console.WriteLine("================================");
-        Console.WriteLine("================================");
-
-
 
         //Methods
 
@@ -128,6 +177,12 @@ class Program
         decimal divide(decimal num1, decimal num2)
         {
             return num1 / num2;
+        }
+
+        //Division
+        decimal modulus(decimal num1, decimal num2)
+        {
+            return num1 % num2;
         }
 
     }
